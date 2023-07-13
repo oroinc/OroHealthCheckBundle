560f795da11e49bc68ffd1f5f2dcf1d8e778ff0e
Eugene Zibrov <106163563+eugene-zibrov@users.noreply.github.com>
2023-07-13 11:09:49 +0300

    BAP-22145: Redis healthchecks do not work (#36005)

--- Tests/Functional/Check/RedisCheckTest.php
+++ Tests/Functional/Check/RedisCheckTest.php
@@ -2,12 +2,19 @@
 
 namespace Oro\Bundle\HealthCheckBundle\Tests\Functional\Check;
 
+use Laminas\Diagnostics\Result\Failure;
 use Laminas\Diagnostics\Result\Success;
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
 class RedisCheckTest extends WebTestCase
 {
+    private array $serviceMap = [
+        'redis_cache' => 'oro.cache.redis_provider',
+        'redis_session_storage' => 'oro_redis_config.session.redis_connection',
+        'redis_doctrine_cache' => 'oro.cache.doctrine.redis_provider'
+    ];
+
     /**
      * {@inheritdoc}
      */
@@ -16,34 +23,75 @@ class RedisCheckTest extends WebTestCase
         $this->initClient([], $this->generateBasicAuthHeader());
     }
 
-    public function testExecuteApiCall()
+    public function testExecuteApiCall(): void
     {
-        $map = [
-            'redis_cache' => 'snc_redis.cache',
-            'redis_session_storage' => 'snc_redis.session',
-            'redis_doctrine_cache' => 'snc_redis.doctrine'
-        ];
+        if (!$this->isRedisConfigured()) {
+            return;
+        }
+
         $container = self::getContainer();
 
-        foreach ($map as $serviceKey => $serviceName) {
-            if (!$container->has($serviceName)) {
-                continue;
-            }
-            $this->client->request(
-                'GET',
-                $this->getUrl('liip_monitor_run_single_check_http_status', ['checkId' => $serviceKey])
+        $this->client->request(
+            'GET',
+            $this->getUrl('liip_monitor_list_checks')
+        );
+
+        $listChecks = json_decode(
+            $this->client->getResponse()->getContent(),
+            JSON_OBJECT_AS_ARRAY
+        );
+
+        foreach ($this->serviceMap as $checkId => $serviceName) {
+            self::assertContains(
+                $checkId,
+                $listChecks,
+                sprintf('Monitor health check checkId %s does not exists', $checkId)
             );
 
-            $this->assertResponseStatusCodeEquals($this->client->getResponse(), Response::HTTP_OK);
+            self::assertTrue(
+                $container->has($serviceName),
+                sprintf('Service %s does not exists', $serviceName)
+            );
+
+            $this->client->request(
+                'GET',
+                $this->getUrl('liip_monitor_run_single_check_http_status', ['checkId' => $checkId])
+            );
+
+            self::assertResponseStatusCodeEquals($this->client->getResponse(), Response::HTTP_OK);
         }
     }
 
-    public function testServiceCheck()
+    public function testSuccessServiceCheck(): void
     {
+        if (!$this->isRedisConfigured()) {
+            return;
+        }
+
         $redisChecks = self::getContainer()->get('oro_health_check.check.redis')->getChecks();
 
         foreach ($redisChecks as $redisCheck) {
             $this->assertInstanceOf(Success::class, $redisCheck->check());
         }
+    }
+
+    public function testFailureServiceCheck(): void
+    {
+        if (!$this->isRedisConfigured()) {
+            $redisChecks = self::getContainer()->get('oro_health_check.check.redis')->getChecks();
+
+            foreach ($redisChecks as $redisCheck) {
+                $this->assertInstanceOf(Failure::class, $redisCheck->check());
+            }
+        }
+    }
+
+    private function isRedisConfigured(): bool
+    {
+        $container = self::getContainer();
+
+        return $container->hasParameter('redis_dsn_cache') &&
+            $container->hasParameter('redis_dsn_doctrine') &&
+            $container->hasParameter('session_handler_dsn');
     }
 }
