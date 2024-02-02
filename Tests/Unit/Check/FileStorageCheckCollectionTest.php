940f2a34fb87d4c47ef08598008e8f6918b376af
Eugene Zibrov <106163563+eugene-zibrov@users.noreply.github.com>
2024-02-02 16:07:49 +0200

    BAP-22072: Health check page contains invalid information (#37461)

--- Tests/Unit/Check/FileStorageCheckCollectionTest.php
+++ Tests/Unit/Check/FileStorageCheckCollectionTest.php
@@ -4,21 +4,31 @@ namespace Oro\Bundle\HealthCheckBundle\Tests\Unit\Check;
 
 use Laminas\Diagnostics\Check\DirWritable;
 use Oro\Bundle\HealthCheckBundle\Check\FileStorageCheckCollection;
+use PHPUnit\Framework\TestCase;
 
-class FileStorageCheckCollectionTest extends \PHPUnit\Framework\TestCase
+class FileStorageCheckCollectionTest extends TestCase
 {
-    public function testGetChecks()
+    public function testGetChecks(): void
     {
         $collection = new FileStorageCheckCollection(
             __DIR__,
-            ['first_key' => __DIR__ . '/../dir/path/test', 'second_key' => '/some/other/dir']
+            [
+                'first_key' => [
+                    'dir' => __DIR__ . '/dir/path/test',
+                    'title' => 'path/test',
+                ],
+                'second_key' => [
+                    'dir' => '/some/other/dir',
+                    'title' => 'other_dir',
+                ]
+            ]
         );
 
-        $check1 = new DirWritable(__DIR__ . '/../dir/path/test');
-        $check1->setLabel(sprintf('Check if "%s" is writable', __DIR__ . '/../dir/path/test'));
+        $check1 = new DirWritable(__DIR__ . '/dir/path/test');
+        $check1->setLabel(sprintf('Check if "%s" directory is writable', 'path/test'));
 
         $check2 = new DirWritable('/some/other/dir');
-        $check2->setLabel('Check if "/some/other/dir" is writable');
+        $check2->setLabel('Check if "other_dir" directory is writable');
 
         $this->assertEquals(
             ['first_key' => $check1, 'second_key' => $check2],
