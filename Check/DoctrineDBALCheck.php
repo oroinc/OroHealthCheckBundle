3cb8552b17f1c2dddefe9c7bfebe25aa46c76756
Michael Bessolov <michael@oroinc.com>
2026-01-09 01:24:49 -0800

    BAP-23252: Upgrade PHP_CodeSniffer and PHP CS Fixer
    - Added missing class PHPDoc comments

--- Check/DoctrineDBALCheck.php
+++ Check/DoctrineDBALCheck.php
@@ -4,6 +4,9 @@ namespace Oro\Bundle\HealthCheckBundle\Check;
 
 use Liip\MonitorBundle\Check\DoctrineDbal;
 
+/**
+ * Health check for Doctrine DBAL availability.
+ */
 class DoctrineDBALCheck extends DoctrineDbal
 {
     #[\Override]
